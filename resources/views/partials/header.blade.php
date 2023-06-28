6c1f8ab134fb3c1e90e8d27a74a5be2212263c12
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-28 17:22:03 +0200

    add nav-bar logo/links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,15 @@
-<header class="p-3">
-    <img class="logo" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Laravel Logo">
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3">
+        <a class="navbar-brand" target="_blank" href="https://www.netflix.com/"><img src="{{ Vite::asset('/resources/img/logo.png') }}" class="img-fluid" style="width: 100px; height: 100px;" alt="Logo"></a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('movies') }}">Movies</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.imdb.com/" target="_blank">Rating by IMDb</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </header>
