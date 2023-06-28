5c0aa504526eb141f28bb60d0b5c2354eca6d3af
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-28 17:20:07 +0200

    fix welcome page  section to iclude a partials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container my-3">
-    @foreach ($movies as $movie)
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">{{ $movie->title }}</h5>
-                <p class="card-text">{{ $movie->original_title }}</p>
-                <p class="card-text">{{ $movie->nationality}}</p>
-                <p class="card-text">{{ $movie->date }}</p>
-                <p class="card-text">{{ $movie->vote }}</p>
-                {{-- <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-primary">View</a> --}}
-            </div>
-        </div>
-    @endforeach
-</div>
+
+@include('partials.moviesCard')
+
 @endsection
