8816f950491959edc71c609648d7d6051601a4f5
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-28 18:49:55 +0200

    fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,10 @@
-<footer class="footer bg-dark text-light text-center p-3">
-    <div class="d-flex justify-content-center gap-3">
+<footer class="footer bg-dark text-light">
+    <div class="d-flex justify-content-center gap-3 p-3">
         <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
         <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
         <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
     </div>
-    <p>&copy; {{ date('Y') }} Movies App. All rights reserved.</p>
+    <div class="text-center">
+        <p>&copy; {{ date('Y') }} Movies App. All rights reserved.</p>
+    </div>
 </footer>
