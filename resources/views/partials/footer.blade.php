ad61c534e1941518627deb42efbdb731f78756e4
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-28 17:21:47 +0200

    add styles and socials link/icon

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,8 @@
-<footer class="p-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</footer>
+<footer class="footer bg-dark text-light text-center p-3">
+    <div class="d-flex justify-content-center gap-3">
+        <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+    </div>
+    <p>&copy; {{ date('Y') }} Movies App. All rights reserved.</p>
+</footer>
