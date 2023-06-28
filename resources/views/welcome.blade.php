25bf756e779801a0c7715f550c9f1dfc4072db63
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-28 18:49:42 +0200

    add style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
-@include('partials.moviesCard')
+@include('partials.moviesList')
 
 @endsection
+
+<style>
+    html,
+    body {
+        height: 100vh;
+    }
+
+    header {
+        height: 142px;
+    }
+
+    footer {
+        height: 100px;
+    }
+
+    .main-content {
+        height: calc(100vh - 142px - 100px);
+        overflow-y: auto;
+    }
+</style>
